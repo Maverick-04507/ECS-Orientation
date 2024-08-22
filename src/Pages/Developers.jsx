a5ba936f4271c3254375ad8575845fd0c5c3c427
Nikhil Singh <164791166+Nikk6174@users.noreply.github.com>
2024-08-22 21:14:26 +0530

    first commit

--- src/Pages/Developers.jsx
+++ src/Pages/Developers.jsx
@@ -3,44 +3,52 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import PersonCard from '../cards/PersonCard';
 const Developers = () => {
-  const developers = [{
-    img: 'https://res.cloudinary.com/dcwqsevti/image/upload/v1694601618/ECS%20PHoto%20members/barnil.jpg',
-    name: 'Barnil Sarma',
+  const developers = [    {
+    img: 'https://res.cloudinary.com/dkquqobzd/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1724184297/Photo_hhrzbd.jpg',
+    name:'Shashwat Patel',
     designation: 'Junior Technical Member',
-    fb: 'https://m.facebook.com/profile.php/?id=100056956539956&name=xhp_nt__fb__action__open_user',
-    insta: '',
-    linkedin: 'https://www.linkedin.com/in/barnil-sarma-34383a255/'
+    fb: 'https://www.facebook.com/profile.php?id=61550363986087 ',
+    insta: 'https://www.instagram.com/belie.ver79/',
+    linkedin: 'https://www.linkedin.com/in/shashwat-patel-5b8a67279?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bfik4FtkKQ6ma9%2BzuASfbMw%3D%3D'
   },
   {
-    img: 'https://res.cloudinary.com/dcwqsevti/image/upload/v1694601616/ECS%20PHoto%20members/pragyan.jpg',
-    name: 'Pragyan Das',
+    img: 'https://res.cloudinary.com/dkquqobzd/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1724185451/Pf_2.1_-_Nikhil_Singh_wsjbev.jpg',
+    name: 'Nikhil Singh',
     designation: 'Junior Technical Member',
-    fb: 'https://www.facebook.com/profile.php?id=100087648530022&mibextid=2JQ9oc',
-    insta: '',
-    linkedin: 'https://www.linkedin.com/in/pragyan-das-197086255'
+    fb: 'https://www.facebook.com/profile.php?id=61550344304871',
+    insta: 'https://www.instagram.com/nikhil_singh2206/',
+    linkedin: 'https://www.linkedin.com/in/nikhilsinghparashar/'
   },
   {
-    img: 'https://res.cloudinary.com/dxdxzcn0q/image/upload/v1694782694/IMG_20230915_182739_pwyd26.jpg',
-    name: 'Garvit Khare',
+    img: 'https://res.cloudinary.com/dkquqobzd/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1724185484/Sibajit_-_SIBAJIT_MAZUMDER_iqy8ky.jpg',
+    name: 'Sibajit Mazumder',
     designation: 'Junior Technical Member',
-    fb: 'https://www.facebook.com/profile.php?id=100087643579805&mibextid=ZbWKwL',
-    insta: '',
-    linkedin: 'https://www.linkedin.com/in/garvit-khare-2297a924b'
+    fb: 'https://www.facebook.com/profile.php?id=61550651010065&mibextid=ZbWKwL ',
+    insta: ' https://www.instagram.com/siibajit_maz.808/',
+    linkedin: 'www.linkedin.com/in/sibajit-mazumder'
   },
   {
-    img: 'https://res.cloudinary.com/dcwqsevti/image/upload/v1694601615/ECS%20PHoto%20members/jagat.jpg',
-    name: 'Jagatjyoti Deb',
+    img: 'https://res.cloudinary.com/dkquqobzd/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1724185452/Profile_-_Subrata_Lodh_qx1wdq.jpg',
+    name: 'Subrata Lodh',
     designation: 'Junior Technical Member',
-    fb: 'https://www.facebook.com/profile.php?id=100084536361943',
+    fb: 'https://www.facebook.com/profile.php?id=100088421967870&mibextid=ZbWKwL ',
     insta: '',
-    linkedin: 'https://www.linkedin.com/in/jagatjyoti-deb-0a57bb223/'
+    linkedin: 'https://www.linkedin.com/in/subrata-lodh-45541328a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'
+  },
+  {
+    img: 'https://res.cloudinary.com/dkquqobzd/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1724184529/IMG-20240319-WA0017_-_DEEPANSHU_SHARMA_vsnu1j.jpg',
+    name: 'Deepanshu Sharma',
+    designation: 'Junior Technical Member',
+    fb: ' https://www.facebook.com/cb.sharma.3304/ ',
+    insta: 'https://www.instagram.com/_dsharmaa',
+    linkedin: 'https://www.linkedin.com/in/deepanshu-sharma-b3a46828a/'
   }
   ];
   return (
-    <div>
+    <div className='container'>
       <Navbar />
       <h1 className='text-[2rem] mt-20 text-[#ffffff] uppercase'>Developers</h1>
-      <div className='flex flex-row justify-center align-center'><PersonCard name="ROHAN JHA" img='https://res.cloudinary.com/dcwqsevti/image/upload/v1694601618/ECS%20PHoto%20members/rohan.jpg' designation="Technical Head" fb="https://www.facebook.com/profile.php?id=100010061770737" insta="" linkedin="https://www.linkedin.com/in/rohan-jha-ab635322b/" /></div>
+      <div className='flex flex-row justify-center align-center'><PersonCard name="Pragyan Das" img='https://res.cloudinary.com/dcwqsevti/image/upload/v1694601616/ECS%20PHoto%20members/pragyan.jpg' designation="Technical Head" fb="https://www.facebook.com/profile.php?id=100087648530022&mibextid=2JQ9oc" insta="" linkedin="https://www.linkedin.com/in/pragyan-das-197086255" /></div>
       <div className='flex flex-wrap justify-around items-center'>
         {developers.map((item) => <PersonCard name={item.name} img={item.img} designation={item.designation} fb={item.fb} insta={item.insta} linkedin={item.linkedin} />)}
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,9 +25,14 @@ const Navbar = (props) => {
     }
   };
   const navs_left = [{ text: 'events', type: 'route', dest: props.events === 'active' ? '#' : '/events' }, { text: 'about us', type: 'route', dest: props.about === 'active' ? '#' : '/about' },{ text: 'members', type: 'route', dest: props.members === 'active' ? '#' : '/members' },];
+
+
   const navs_right = [ { text: 'developers', type: 'route', dest: props.developers === 'active' ? '#' : '/developers' }, { text: 'resource hub', type: 'route', dest: '/resourcehub' }];
-  const [mobileMenu, setMenu] = useState('0vh');
-  return (
+
+  
+const [mobileMenu, setMenu] = useState('0vh');
+
+return (
     <>
     {openLogin && (
         <Login
@@ -48,6 +53,7 @@ const Navbar = (props) => {
         <LinkRoute to='/'><img src={Logo} alt="" className='h-10 w-24' /></LinkRoute>
         <img src={mobileMenu === '0vh' ? 'https://res.cloudinary.com/dhry5xscm/image/upload/v1701946021/ecs-website/hamburger-menu_iqxqgn.png' : 'https://res.cloudinary.com/dhry5xscm/image/upload/v1701946204/ecs-website/xmark-solid_m8hqyk_vb8jx6.svg'} alt="" className='h-7 w-7' onClick={() => mobileMenu === '0vh' ? setMenu('100vh') : setMenu('0vh')} style={{ filter: `invert(${mobileMenu === '0vh' ? '0%' : '100%'})` }} />
       </div>
+      
       <div className='flex flex-col bg-[#000055dd] justify-between items-center p-[1rem] w-[50%] md:invisible md:h-0' style={{ zIndex: '5', backdropFilter: 'blur(2px)', left: '10vw', position: 'absolute', width: '90vw', opacity: `${mobileMenu === '0vh' ? '0' : '1'}`, height: `${mobileMenu}`, transition: '500ms ease' }}>
         <LinkRoute to='/'><img src='https://res.cloudinary.com/dhry5xscm/image/upload/v1701942813/ecs-website/ecs-profile_xilte1.svg' style={{ scale: `${mobileMenu === '0vh' ? '0' : '1'}` }} onClick={handleClick} /></LinkRoute>
         {navs_left.map((item) => <NavLink text={item.text} type={item.type} dest={item.dest} state={mobileMenu} />)}
