5b7d622e3dfb590199c1a94da262671b072776f9
Subrata-20 <subrata_ug_23@ece.nits.ac.in>
2024-08-23 02:05:52 +0530

    Carousal change

--- src/components/Carousal2.jsx
+++ src/components/Carousal2.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import { Autoplay, EffectCoverflow } from 'swiper/modules';
-import Dehleez from "../images/glimpse.png"
-// import './styles.css'
-import './carousal2.css';
 
 const Carousal2 = () => {
     const events_data = [
-        { 'name': 'ORIENTATION', 'image': 'https://res.cloudinary.com/dhry5xscm/image/upload/v1701964299/ecs-website/img-orientation_n6p25q.jpg' },
-        { 'name': 'FRESHERS', 'image': 'https://res.cloudinary.com/dhry5xscm/image/upload/v1701964299/ecs-website/img-spectrum_jpf9tn.jpg' },
-        { 'name': 'DEHLEEZ', 'image': 'https://res.cloudinary.com/dhry5xscm/image/upload/v1701964294/ecs-website/img-dehleez_xfpidm.jpg' },
-        { 'name': 'SPECTARE', 'image': 'https://res.cloudinary.com/dhry5xscm/image/upload/v1701964294/ecs-website/img-spectre_iblbgs.jpg' },
+        { 'name': 'ORIENTATION', 'image': 'https://res.cloudinary.com/dsj9gr1o3/image/upload/v1724355785/Orientation2_hpanff.jpg' },
+        { 'name': 'FRESHERS', 'image': 'https://res.cloudinary.com/dsj9gr1o3/image/upload/v1724355786/Freshers2_cypjde.jpg' },
+        { 'name': 'DEHLEEZ', 'image': 'https://res.cloudinary.com/dsj9gr1o3/image/upload/v1724356477/Dehleez2_1_t2ujvu.jpg' },
+        { 'name': 'SPECTRE', 'image': 'https://res.cloudinary.com/dsj9gr1o3/image/upload/v1724355785/Event_ob89wa.jpg' },
+        { 'name': 'ORIENTATION', 'image': 'https://res.cloudinary.com/dsj9gr1o3/image/upload/v1724355786/Orientation_tmwhim.jpg'},
+        { 'name': 'FRESHERS', 'image': 'https://res.cloudinary.com/dsj9gr1o3/image/upload/v1724355786/Freshers_rosq4b.jpg' },
+        { 'name': 'SPEAKER SESSION', 'image': 'https://res.cloudinary.com/dsj9gr1o3/image/upload/v1724355786/Speakersess_lmlwvl.jpg' },
+        { 'name': 'EIC', 'image': 'https://res.cloudinary.com/dsj9gr1o3/image/upload/v1724355556/EIC_irzhje.jpg' },
     ];
+   
     return (
-        <div className='w-full'>
+        <div className='max-w-screen-xl flex justify-center items-center overflow-hidden mt-12'>
             <Swiper
-                autoplay={{ delay: 2000 }}
+                autoplay={{ delay: 4000 }}
                 effect={'coverflow'}
                 grabCursor={true}
                 centeredSlides={true}
@@ -29,31 +31,22 @@ const Carousal2 = () => {
                     depth: 100,
                     modifier: 2.5,
                 }}
-
                 modules={[EffectCoverflow, Autoplay]}
-
-                className="swiper_container">
-                {events_data.map((item) =>
-                    <SwiperSlide>
-                        <img src={item.image} alt="Loading image..." />
-                    </SwiperSlide>)
-                }
-                {/* <SwiperSlide>
-                    <img src={Dehleez} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={Dehleez} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={Dehleez} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={Dehleez} alt="" />
-                </SwiperSlide> */}
-
+                className="w-full h-full md:scale-100 scale-50"
+            >
+                {events_data.map((item, index) => (
+                    <SwiperSlide key={index}>
+                        <img
+                            src={item.image}
+                            alt={item.name}
+                            className="object-cover rounded-lg"
+                            style={{width:'40vw', height:'55vh' }}
+                        />
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </div>
-    )
+    );
 }
 
-export default Carousal2
+export default Carousal2;
