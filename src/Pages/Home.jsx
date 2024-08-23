d8fc35222eb467fd18e4f0be1c6cd962403882a8
Sibajit Mazumder <sibajit.mazumdar@gmail.com>
2024-08-23 14:56:54 +0530

    Landing Page initial update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Events from '../components/EventsCarousal'
@@ -8,23 +8,44 @@ import Alumni from '../components/Alumni'
 import Footer from '../components/Footer'
 import {Link} from 'react-router-dom'
 import './spectrumCont.css'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import bgVid from "../images/BackgroundVideo.mp4"
+import bgimg from "../images/digi.png"
+const BgURl="linear-gradient(45deg, #d80d0d, transparent)";
 const Home = () => {
+  const [mobileView,setmobileView]=useState(true)
+  // console.log(mobileView)
   return (
-    <div className='flex flex-col' style={{ rowGap: '5rem' }}>
-      <Navbar home='active' events='inactive' developers='inactive' about='inactive' feed='inactive' members='inactive' />
-      <Hero />
-      <div>
-        <Link to='/cos'><img  className='w-[40%] h-[30rem] m-auto -mt-20' src="https://res.cloudinary.com/dyuggtbjl/image/upload/v1705428403/ecs/b0a3db62-abe8-490b-8d35-3c6697aba161_drup1r.jpg" alt="" /></Link>
-      </div>
-      <div className='cardHome p-[1rem] bg-[#ffffff11]'>
-        <Link to='/Spectrum'>Checkout our official Spectrum 10.0 page</Link>
-      </div>
-      <Events />
-      <Messages />
-      <Gallery />
-      <Alumni />
-      <Footer />
-    </div>
+    <Parallax pages={2.8}  >
+        <ParallaxLayer speed={0.5} offset={0} className='z-10'>
+        <div className="gradient z-20"></div>
+        <video id='background-video' src={bgVid} autoPlay loop muted={true} className='w-screen z-10 m-auto bg-contain '/>
+        </ParallaxLayer>
+        <ParallaxLayer speed={0.5} className='flex flex-col justify-center z-10'> 
+         <Navbar home='active' events='inactive' developers='inactive' about='inactive' feed='inactive' members='inactive' className="bg-transparent " />
+            <Hero />
+            <div className='cardHome p-[1rem] bg-[#ffffff11]'>
+              <Link to='/Spectrum' className='z-50'>Checkout our official Spectrum 10.0 page</Link>
+            </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0.99} speed={0.6}  factor={3} className=' pt-48' id='bgImg' style={{ backgroundImage:`  linear-gradient(#060027, #0600276d,#000000),url(${bgimg})`,backgroundSize:"cover",top:"10px",backgroundPosition:"center"}} >
+          <ParallaxLayer offset={0.5} speed={0.6} 
+          style={{inset:`${mobileView?'30vh 0':'0vh 0'}` }}
+          >
+              <Events />
+          </ParallaxLayer>
+        <ParallaxLayer offset={1.01} speed={0.65} >
+          <ParallaxLayer offset={0.5} speed={0.6}>
+            <Messages />
+          </ParallaxLayer>
+        </ParallaxLayer>
+        {/* <ParallaxLayer offset={3.9}><Gallery /></ParallaxLayer> */}
+        <ParallaxLayer offset={1.99} speed={1}><Alumni />
+        
+        <ParallaxLayer offset={0.79} speed={0} ><Footer /></ParallaxLayer>
+        </ParallaxLayer>
+        </ParallaxLayer>
+      </Parallax>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,7 +48,7 @@ return (
           setCurrentUser={setCurrentUser}
         />
       )}
-    <div style={{ overflowX: 'hidden' }}>
+    <div style={{ overflowX: 'hidden' ,}}>
       <div className='flex visible justify-between pl-[1rem] pr-[1rem] h-[5rem] items-center md:invisible md:h-0'>
         <LinkRoute to='/'><img src={Logo} alt="" className='h-10 w-24' /></LinkRoute>
         <img src={mobileMenu === '0vh' ? 'https://res.cloudinary.com/dhry5xscm/image/upload/v1701946021/ecs-website/hamburger-menu_iqxqgn.png' : 'https://res.cloudinary.com/dhry5xscm/image/upload/v1701946204/ecs-website/xmark-solid_m8hqyk_vb8jx6.svg'} alt="" className='h-7 w-7' onClick={() => mobileMenu === '0vh' ? setMenu('100vh') : setMenu('0vh')} style={{ filter: `invert(${mobileMenu === '0vh' ? '0%' : '100%'})` }} />
