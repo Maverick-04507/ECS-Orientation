af9159f93748e1385c31e67f142f11e8025086f9
Shashwat Patel <165941633+Maverick-04507@users.noreply.github.com>
2024-08-23 17:05:30 +0530

    Update About.jsx
    Improved About Section

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,10 +7,10 @@ import Footer from "../components/Footer"
 const About = () => {
   return (
     // <div className='bg-gradient-to-b from-blue-900 via-blue-600 to-blue-300'>
-    <div className='bg-[#0e005c]'>
+    <div className='container'>
       <div>
         <Navbar />
-        <h1 className='mt-[9rem] mb-10 text-4xl font-semibold text-white'>KNOW <span className=''>ABOUT US</span></h1>
+        <h1 className='mt-[9rem] mb-10 text-4xl font-semibold text-white'>KNOW <span className='className='text-yellow-400 font-extrabold''>ABOUT US</span></h1>
         <div><Carousal2 /></div>
         <p className='mt-20 flex m-auto w-[90%] text-lg leading-8 mb-[9rem] text-white'>The Electronics & Communication Society is the branch society which represents the department of ECE. The prime purpose of this society is to make the students familiar with the branch. Besides that, a number of exciting events take place under ECS. Starting from the branch Orientation, several events take place including Utkrishtha, the Freshers', the Spectrum that is the annual flagship event, Spectrum Fit for different indoor and outdoor activities and lastly, Dehleez, the farewell.</p>
       </div>
